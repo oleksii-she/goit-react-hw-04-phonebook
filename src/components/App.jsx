be886122e723b6979ac9bb0e42e3f61dca5b4f2e
Oleksii Shevchenko <vazerius@gmail.com>
2022-10-27 17:23:44 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,7 @@ import { FormData } from './form/form';
 import { Filter } from './filter/Filter';
 import { ContactsList } from './contacts/contactsList';
 import { Conteiner } from './conteiner.styled';
-
-const contactsData = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+import { contactsData } from './form/contactsData';
 const LS_KEY = 'contact';
 export const App = () => {
   const [contacts, setContacts] = useState(() => {
